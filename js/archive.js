970b6a6adf4e5d12687f74b5cba44e7d88d6ec83
Kiri M. Kvistnes <71313020+kirikvi@users.noreply.github.com>
2021-05-21 22:12:15 +0200

    styling and media queries

--- js/archive.js
+++ js/archive.js
@@ -1,4 +1,4 @@
-const url = "https://kingdomofnorway.kvistnes.one/wp-json/wp/v2/posts?per_page=15&_embed";
+const url = "https://kingdomofnorway.kvistnes.one/wp-json/wp/v2/posts?per_page=15";
 const archive = document.querySelector(".archive");
 const viewMore = document.querySelector("#viewmore");
 const buttonContainer = document.querySelector(".button-container");
@@ -8,17 +8,24 @@ async function fetchPosts(){
     try {
         const search = await fetch(url);
         const results = await search.json();
-        console.log(results);  
+          
         
       // view 10 blog posts  
         for(let i = 0; i < 10; i++) {
             const postList = results[i];
+            console.log(results[i]);
+            
+            const shortText = postList.excerpt.rendered.substring(0,135) + "...";
 
             archive.innerHTML += `
             <a href="post.html?id=${postList.id}">
                 <div class="archive-item">
                     <p>${postList.content.rendered}</p>
-                    <h2>${postList.title.rendered}</h2>
+                    <div class="archive-text">
+                        <h2>${postList.title.rendered}</h2>
+                        <p>${shortText}</p>
+                        <a href="post.html?id=${postList.id}">Read more...</a>
+                    <div>
                 </div>
             </a>`;
             
@@ -32,14 +39,21 @@ async function fetchPosts(){
         // view more posts when clicking the button
         viewMore.onclick = function viewMorePosts(){
             for(var i = 10; i < results.length + 10; i++){
+
+                const shortText = results[i].excerpt.rendered.substring(0,135) + "...";
+
                 archive.innerHTML += `
                 <a href="post.html?id=${results[i].id}">
-                    <div class="archive-item">
-                        <p>${results[i].content.rendered}</p>
+                <div class="archive-item">
+                    <p>${results[i].content.rendered}</p>
+                    <div class="archive-text">
                         <h2>${results[i].title.rendered}</h2>
-                    </div>
+                        <p>${shortText}</p>
+                        <a href="post.html?id=${results[i].id}">Read more...</a>
+                    <div>
+                </div>
                 </a>`;
-
+               
                 //hide button and add text when the last post is reached
                 if(results.length) {
                     viewMore.style.display = "none";
